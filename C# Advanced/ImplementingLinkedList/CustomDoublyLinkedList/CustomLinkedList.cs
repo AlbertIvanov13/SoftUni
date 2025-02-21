db22976f0791d496a516500ed89cc22abac385ba
Albert Ivanov <albertivanov187@gmail.com>
2025-02-21 23:18:18 +0200

    Update

---
+++ C# Advanced/ImplementingLinkedList/CustomDoublyLinkedList/CustomLinkedList.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    public class CustomLinkedList
+    {
+        private class Node
+        {
+            public int Value { get; set; }
+
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+
+            public Node(int value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new Node(element);
+            }
+            else
+            {
+                var newHead = new Node(element);
+                newHead.Next = this.head;
+                this.head.Previous = newHead;
+                this.head = newHead;
+            }
+            this.Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.tail = this.head = new Node(element);
+            }
+            else
+            {
+                var newTail = new Node(element);
+                newTail.Previous = this.tail;
+                this.tail.Next = newTail;
+                this.tail = newTail;
+            }
+            this.Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty!");
+            }
+
+            var firstElement = this.head.Value;
+            this.head = this.head.Next;
+            if (this.head != null)
+            {
+                this.head.Previous = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+            this.Count--;
+            return firstElement;
+        }
+
+        public int RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty!");
+            }
+            
+            var lastElement = this.tail.Value;
+            this.tail = this.tail.Previous;
+            if (this.tail != null)
+            {
+                this.tail.Next = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+            this.Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[this.Count];
+            int counter = 0;
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                array[counter] = currentNode.Value;
+                currentNode = currentNode.Next;
+                counter++;
+            }
+            return array;
+        }
+    }
+}
