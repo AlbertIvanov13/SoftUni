a7a51383dcd89b46b7d33966917893b165da6b55
Albert Ivanov <albertivanov187@gmail.com>
2025-02-23 23:52:51 +0200

    Custom stack

--- C# Advanced/CustomDataStructures/CustomDataStructures/CustomList/obj/Debug/net6.0/CustomList.AssemblyInfo.cs
+++ C# Advanced/CustomDataStructures/CustomDataStructures/CustomList/obj/Debug/net6.0/CustomList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d6887f084b8a0d4476480a7384047302854158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63697f01bfe48f358b88d0fd0682476846add13")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ C# Advanced/CustomDataStructures/CustomDataStructures/CustomStack/CustomStack/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomStack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
