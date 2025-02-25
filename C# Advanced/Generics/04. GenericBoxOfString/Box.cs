efbeb3977439ed44ea3a4369b5e2a29587783d28
Albert Ivanov <albertivanov187@gmail.com>
2025-02-26 01:38:22 +0200

    GenericBoxOfString

---
+++ C# Advanced/Generics/04. GenericBoxOfString/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public T Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.item}";
+        }
+
+    }
+}
