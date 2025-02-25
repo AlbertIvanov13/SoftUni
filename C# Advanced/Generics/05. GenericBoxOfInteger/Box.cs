68468e7c783fcf9574fdb561b090745983807d0c
Albert Ivanov <albertivanov187@gmail.com>
2025-02-26 01:44:41 +0200

    GenericBoxOfInteger

---
+++ C# Advanced/Generics/05. GenericBoxOfInteger/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public T Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {this.item}";
+        }
+    }
+}
