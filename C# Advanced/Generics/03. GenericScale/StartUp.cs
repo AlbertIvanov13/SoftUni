7e7d091d15de589e838867eec003e7b6d1071248
Albert Ivanov <albertivanov187@gmail.com>
2025-02-26 00:30:12 +0200

    GenericScale

---
+++ C# Advanced/Generics/03. GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            EqualityScale<int> equalityScale = new EqualityScale<int>(5, 2);
+
+            Console.WriteLine(equalityScale.AreEqual());
+        }
+    }
+}
