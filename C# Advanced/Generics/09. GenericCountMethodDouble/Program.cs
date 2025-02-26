78bacff6fd6c9ba35f6672ac31cc2f4568093107
Albert Ivanov <albertivanov187@gmail.com>
2025-02-26 12:10:38 +0200

    GenericCountMethodDouble

---
+++ C# Advanced/Generics/09. GenericCountMethodDouble/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCountMethodDouble
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<double> items = new List<double>();
+            for (int i = 0; i < n; i++)
+            {
+                double element = double.Parse(Console.ReadLine());
+                items.Add(element);
+            }
+
+            double itemToCompare = double.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>(itemToCompare);
+
+            Console.WriteLine(box.CompareTo(items, itemToCompare));
+        }
+    }
+}
