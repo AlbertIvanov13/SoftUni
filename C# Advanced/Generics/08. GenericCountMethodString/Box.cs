07e5e97737215e7b85d2d3c3e8d63904db3c25d1
Albert Ivanov <albertivanov187@gmail.com>
2025-02-26 12:04:12 +0200

    GenericCountMethodString

---
+++ C# Advanced/Generics/08. GenericCountMethodString/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodString
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private T item;
+
+        public T Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public int CompareTo(List<T> list, T element)
+        {
+            int counter = 0;
+
+            foreach (var item in list)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ C# Advanced/Generics/08. GenericCountMethodString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCountMethodString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> items= new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string element = Console.ReadLine();
+                items.Add(element);
+            }
+
+            string itemToCompare = Console.ReadLine();
+            Box<string> box = new Box<string>(itemToCompare);
+
+            Console.WriteLine(box.CompareTo(items, itemToCompare));
+        }
+    }
+}
