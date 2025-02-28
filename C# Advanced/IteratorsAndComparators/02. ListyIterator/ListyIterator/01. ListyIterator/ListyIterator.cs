5b0cfb1e30f8e78b39e4c4b3ac1a1b2c53964386
Albert Ivanov <albertivanov187@gmail.com>
2025-02-28 22:59:28 +0200

    Implement ListyIterator

---
+++ C# Advanced/IteratorsAndComparators/02. ListyIterator/ListyIterator/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> items;
+        private int currentIndex = 0;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = new List<T>(items);
+        }
+
+        public List<T> Items
+        {
+            get { return items; }
+            set { items = value; }
+        }
+
+        public bool Move()
+        {
+            for (int i = this.currentIndex; i < this.items.Count; i++)
+            {
+                if (i < this.items.Count - 1)
+                {
+                    this.currentIndex++;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            for (int i = this.currentIndex; i < this.items.Count; i++)
+            {
+                if (i < this.items.Count - 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            try
+            {
+                Console.WriteLine(this.items[this.currentIndex]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/02. ListyIterator/ListyIterator/01. ListyIterator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ListyIterator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] createCommand = Console.ReadLine().Split(" ");
+            List<string> list = new List<string>(createCommand[1..]);
+            ListyIterator<string> strings = new ListyIterator<string>(list);
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(strings.Move());
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(strings.HasNext());
+                }
+                else if (command == "Print")
+                {
+                    strings.Print();
+                }
+            }
+        }
+    }
+}
