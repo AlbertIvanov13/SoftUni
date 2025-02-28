ca44a416426faf99f431c9a958838d9d69c3bfb5
Albert Ivanov <albertivanov187@gmail.com>
2025-03-01 01:37:58 +0200

    Froggy

---
+++ C# Advanced/IteratorsAndComparators/Froggy/Lake.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> numbers;
+        private bool isOver = false;
+
+        public Lake(List<int> numbers)
+        {
+            this.numbers = numbers;
+        }
+        public List<int> Numbers
+        {
+            get { return numbers; }
+            set { numbers = value; }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.numbers.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return this.numbers[i];
+                }
+
+                if (i == this.numbers.Count - 1)
+                {
+                    this.isOver = true;
+                }
+            }
+
+            if (this.isOver)
+            {
+                for (int i = this.numbers.Count - 1; i >= 0; i--)
+                {
+                    if (i % 2 != 0)
+                    {
+                        yield return this.numbers[i];
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/Froggy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace Froggy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+
+            Lake lake = new Lake(list);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
