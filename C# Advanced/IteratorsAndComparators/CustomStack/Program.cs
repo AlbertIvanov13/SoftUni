c335fa4567959761899b6b3b2c03fc12d5d2ef4b
Albert Ivanov <albertivanov187@gmail.com>
2025-03-01 00:20:03 +0200

    Implementing stack

---
+++ C# Advanced/IteratorsAndComparators/CustomStack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Threading.Channels;
+
+namespace CustomStack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] createCommand = Console.ReadLine().Split(new string[] { " ", ", "},StringSplitOptions.RemoveEmptyEntries);
+            Stack<string> stack = new Stack<string>(createCommand[1..]);
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] inputs = command.Split();
+                if (inputs[0] == "Pop")
+                {
+                    stack.Pop();
+                }
+                else if (inputs[0] == "Push")
+                {
+                    stack.Push(inputs[1]);
+                }
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/CustomStack/Stack.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private const int InitialCapacity = 4;
+        private T[] items;
+
+        public Stack(params T[] items)
+        {
+            this.items = items;
+            this.Count = items.Length;
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                IndexOutOfRangeException(index);
+                return this[index];
+            }
+            set
+            {
+                IndexOutOfRangeException(index);
+                this[index] = value;
+            }
+        }
+
+        public void Push(T element)
+        {
+            if (this.items.Length == this.Count)
+            {
+                Resize();
+            }
+
+            this.items[this.Count] = element;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            int index = this.Count - 1;
+            try
+            {
+                var lastIndex = this.Count - 1;
+                T last = this.items[lastIndex];
+                this.Count--;
+                return last;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No elements");
+            }
+
+            return default;
+        }
+
+        public T Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty!");
+            }
+
+            var lastIndex = this.Count - 1;
+            T last = this.items[lastIndex];
+            return last;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+
+        private void IndexOutOfRangeException(int index)
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Index out of range!");
+            }
+        }
+
+        private void Resize()
+        {
+            T[] copyArray = new T[this.items.Length * 2];
+
+            for (int i = 0; i < this.items.Length; i++)
+            {
+                copyArray[i] = this.items[i];
+            }
+            this.items = copyArray;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
