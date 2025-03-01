7229ff8a355f8d000df948e9e76c3ac5d531a4c9
Albert Ivanov <albertivanov187@gmail.com>
2025-03-01 23:28:54 +0200

    100/100

---
+++ C# Advanced/IteratorsAndComparators/EqualityLogic/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+		private string name;
+		private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+
+        public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            if (result != 0)
+            {
+                return result;
+            }
+
+            result = this.Age.CompareTo(other.Age);
+
+            if (result != 0)
+            {
+                return result;
+            }
+
+            return result;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Person other)
+            {
+                return Name == other.Name && Age == other.Age;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Age);
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/EqualityLogic/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace EqualityLogic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<Person> hashSet = new HashSet<Person>();
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+
+            List<Person> list = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                Person person = new Person(input[0], int.Parse(input[1]));
+                list.Add(person);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (list[i].Equals(list[j]))
+                    {
+                        if (!hashSet.Contains(list[i]) && !sortedSet.Contains(list[i]))
+                        {
+                            hashSet.Add(list[i]);
+                            sortedSet.Add(list[i]);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(hashSet.Count);
+            Console.WriteLine(sortedSet.Count);
+        }
+    }
+}
