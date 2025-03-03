c6c13e070c248e30b099b3769cbe51c186d7cd0f
Albert Ivanov <albertivanov187@gmail.com>
2025-03-03 23:52:49 +0200

    StackOfStirngs

---
+++ C# OOP/Inheritance/StackOfStrings/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.AddRange(new[] { "1", "2", "3", "4", "5" });
+
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
