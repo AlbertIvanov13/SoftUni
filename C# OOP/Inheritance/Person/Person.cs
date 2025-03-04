2407844089653b0e62c13c66c830885000117258
Albert Ivanov <albertivanov187@gmail.com>
2025-03-04 16:13:08 +0200

    Person

---
+++ C# OOP/Inheritance/Person/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+		private string name;
+		private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		public virtual int Age
+		{
+			get 
+			{
+				return age;
+			}
+			set 
+			{
+				if (value > 0)
+				{
+					age = value; 
+				}
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{this.GetType().Name} -> Name: {Name}, Age: {Age}";
+		}
+
+	}
+}
