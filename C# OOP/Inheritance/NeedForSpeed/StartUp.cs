52493f79796b16988f50a4dc9bb6251876fa78cc
Albert Ivanov <albertivanov187@gmail.com>
2025-03-04 17:49:51 +0200

    NeedForSpeed 100/100

---
+++ C# OOP/Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            FamilyCar sportCar = new FamilyCar(300, 100);
+            sportCar.Drive(10);
+            Console.WriteLine(sportCar.Fuel);
+        }
+    }
+}
