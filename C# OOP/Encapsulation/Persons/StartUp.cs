acaf98e44cfc17a8632cb36d8a93bdfb02fcfc1a
Albert Ivanov <albertivanov187@gmail.com>
2025-03-05 21:07:33 +0200

    Person 100/100

---
+++ C# OOP/Encapsulation/Persons/StartUp.cs
@@ -0,0 +1,24 @@
+﻿
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] arguments = Console.ReadLine().Split();
+                var person = new Person(arguments[0], arguments[1], int.Parse(arguments[2]));
+                persons.Add(person);
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p));
+        }
+    }
+}
