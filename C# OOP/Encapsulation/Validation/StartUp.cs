db5ae0baf9d55e07b685460fb7362b1d0e154077
Albert Ivanov <albertivanov187@gmail.com>
2025-03-05 22:04:58 +0200

    Validation 100/100

---
+++ C# OOP/Encapsulation/Validation/StartUp.cs
@@ -0,0 +1,29 @@
+﻿
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                try
+                {
+                    Person person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2]), decimal.Parse(personInfo[3]));
+                    persons.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            var percentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(percentage));
+            persons.ForEach(p => Console.WriteLine(p));
+        }
+    }
+}
