82e9d449a7747c55e5b2399f4f6ee5f12c1c6d96
Albert Ivanov <albertivanov187@gmail.com>
2025-03-06 00:00:52 +0200

    Team 100/100

---
+++ C# OOP/Encapsulation/Team/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                try
+                {
+                    Person person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2]), decimal.Parse(personInfo[3]));
+                    persons.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Team team = new Team("SoftUni");
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"{team.FirstTeam.Count}");
+            Console.WriteLine($"{team.ReserveTeam.Count}");
+        }
+    }
+}
