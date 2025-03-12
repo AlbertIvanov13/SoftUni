d22c6a54c171b5fc1694a4eb009db4091825ed5b
Albert Ivanov <albertivanov187@gmail.com>
2025-03-12 23:31:58 +0200

    EnterNumbers 100/100

---
+++ C# OOP/ExceptionsAndErrorHandling/EnterNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.AccessControl;
+
+namespace EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int start = 1;
+            int end = 12;
+            int counter = 0;
+            while (counter < 10)
+            {
+                counter++;
+                try
+                {
+                    int number = ReadNumber(start, end);
+                    numbers.Add(number);
+                    start = number;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    counter--;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        private static int ReadNumber(int start, int end)
+        {
+            string number = Console.ReadLine();
+
+            if (number.All(d => char.IsLetter(d)))
+            {
+                throw new ArgumentException("Invalid Number!");
+            }
+
+            if (int.Parse(number) <= start || int.Parse(number) > 100)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+
+            return int.Parse(number);
+        }
+    }
+}
