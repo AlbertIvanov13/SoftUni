c88e2108f4d684597df2484ac499e570910cd776
Albert Ivanov <albertivanov187@gmail.com>
2025-03-14 23:16:58 +0200

    Single responsibility example

---
+++ C# OOP/SOLID/SingleResponsibility/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SingleResponsibility
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = ReadNumbers();
+            var filteredNumbers = FilterNumbers(numbers);
+            var sortedNumbers = SortNumbers(filteredNumbers);
+            PrintNumbers(sortedNumbers);
+        }
+
+        static int[] ReadNumbers()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            return numbers;
+        }
+
+        static int[] FilterNumbers(int[] numbers)
+        {
+            numbers = numbers.Where(n => n % 2 == 0).ToArray();
+            return numbers;
+        }
+
+        static int[] SortNumbers(int[] numbers)
+        {
+            numbers = numbers.OrderByDescending(n => n).ToArray();
+            return numbers;
+        }
+
+        static void PrintNumbers(int[] numbers)
+        {
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
