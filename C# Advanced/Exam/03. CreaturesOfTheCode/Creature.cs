17361c35f851defda42816abe4ad1445d5bfd62b
Albert Ivanov <albertivanov187@gmail.com>
2025-03-16 00:06:39 +0200

    CreaturesOfTheCode 100/100

---
+++ C# Advanced/Exam/03. CreaturesOfTheCode/Creature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._CreaturesOfTheCode
+{
+    public class Creature
+    {
+        public string Name { get; set; }
+        public string Kind { get; set; }
+        public int Health { get; set; }
+        public List<string> Abilities { get; set; }
+        public Creature(string name, string kind, int health, string abilities)
+        {
+            Name = name;
+            Kind = kind;
+            Health = health;
+            Abilities = new List<string>(abilities.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList());
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Name} ({Kind}) has {Health} HP");
+            sb.AppendLine($"Abilities: {string.Join(", ", Abilities)}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Exam/03. CreaturesOfTheCode/MythicalCreaturesHub.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._CreaturesOfTheCode
+{
+    public class MythicalCreaturesHub
+    {
+        public List<Creature> Creatures { get; set; }
+        public int Capacity { get; set; }
+
+        public MythicalCreaturesHub(int capacity)
+        {
+            Capacity = capacity;
+            Creatures = new List<Creature>(capacity);
+        }
+
+        public void AddCreature(Creature creature)
+        {
+            if (Creatures.Count < Capacity && !Creatures.Any(c => c.Name.ToLower() == creature.Name.ToLower()))
+            {
+                Creatures.Add(creature);
+            }
+        }
+
+        public bool RemoveCreature(string name)
+        {
+            if (Creatures.Any(c => c.Name == name))
+            {
+                Creatures.Remove(Creatures.First(c => c.Name == name));
+                return true;
+            }
+
+            return false;
+        }
+
+        public Creature GetStrongestCreature()
+        {
+            Creatures = Creatures.OrderByDescending(c => c.Health).ToList();
+            return Creatures[0];
+        }
+
+        public string Details(string creatureName)
+        {
+            if (Creatures.Any(c => c.Name == creatureName))
+            {
+                Creature creature = Creatures.FirstOrDefault(c => c.Name == creatureName);
+                return creature.ToString();
+            }
+
+            return $"Creature with the name {creatureName} not found.";
+        }
+
+        public string GetAllCreatures()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Mythical Creatures:");
+            Creatures = Creatures.OrderBy(c => c.Name).ToList();
+            foreach (Creature creature in Creatures)
+            {
+                sb.AppendLine($"{creature.Name} -> {creature.Kind}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
