f426d11a83410eb2225f699d69a2dba3c148a165
Albert Ivanov <albertivanov187@gmail.com>
2025-03-19 01:04:59 +0200

    CocktailBar 100/100

---
+++ C# Advanced/Exam/09. CocktailBar/Cocktail.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace CocktailBar
+{
+    public class Cocktail
+    {
+        private List<string> _ingredients;
+
+        public Cocktail(string name, decimal price, double volume, string ingredients)
+        {
+            Name = name;
+            Price = price;
+            Volume = volume;
+            _ingredients = ingredients.Split(", ").ToList();
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public double Volume { get; set; }
+
+        public List<string> Ingredients { get => _ingredients; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Name}, Price: {Price:f2} BGN, Volume: {Volume} ml");
+            sb.AppendLine($"Ingredients: {string.Join(", ", Ingredients)}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/Exam/09. CocktailBar/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace CocktailBar
+{
+    public class Menu
+    {
+        public Menu(int barCapacity)
+        {
+            BarCapacity = barCapacity;
+            Cocktails = new List<Cocktail>(barCapacity);
+        }
+        public List<Cocktail> Cocktails { get; set; }
+        public int BarCapacity { get; set; }
+
+        public void AddCocktail(Cocktail cocktail)
+        {
+            if (Cocktails.Count < BarCapacity && !Cocktails.Any(c => c.Name == cocktail.Name))
+            {
+                Cocktails.Add(cocktail);
+            }
+        }
+
+        public bool RemoveCocktail(string name)
+        {
+            if (Cocktails.Any(c => c.Name == name))
+            {
+                Cocktails.Remove(Cocktails.First(c => c.Name == name));
+                return true;
+            }
+
+            return false;
+        }
+
+        public Cocktail GetMostDiverse()
+        {
+            Cocktail cocktail = Cocktails.MaxBy(c => c.Ingredients.Count);
+            return cocktail;
+        }
+
+        public string Details(string cocktailName)
+        {
+            Cocktail cocktail = Cocktails.FirstOrDefault(c => c.Name == cocktailName);
+            return cocktail.ToString();
+        }
+
+        public string GetAll()
+        {
+            StringBuilder sb = new StringBuilder();
+            Cocktails = Cocktails.OrderBy(c => c.Name).ToList();
+            sb.AppendLine($"All Cocktails:");
+            foreach (Cocktail cocktail in Cocktails)
+            {
+                sb.AppendLine($"{cocktail.Name}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
