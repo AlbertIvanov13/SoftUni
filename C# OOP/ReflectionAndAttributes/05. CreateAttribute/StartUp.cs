23bf13a691cfcd1ada759a5b1eeaf57dc03a1abd
Albert Ivanov <albertivanov187@gmail.com>
2025-03-20 01:59:17 +0200

    CreateAttribute 100/100

---
+++ C# OOP/ReflectionAndAttributes/05. CreateAttribute/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace AuthorProblem
+{
+    [Author("Victor")]
+    public class StartUp
+    {
+        [Author("George")]
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
