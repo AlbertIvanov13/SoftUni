83f62536aabc8281583aa17d9a1c85c91e7c467b
Albert Ivanov <albertivanov187@gmail.com>
2025-03-21 16:50:53 +0200

    CodeTracker

---
+++ C# OOP/ReflectionAndAttributes/06. CodeTracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    public class StartUp
+    {
+        [Author("George")]
+        public static void Main()
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
