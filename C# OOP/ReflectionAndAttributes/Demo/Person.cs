387d4035311344902fce8b042050cf5d67be3362
Albert Ivanov <albertivanov187@gmail.com>
2025-03-21 21:36:10 +0200

    Accessing methods of a class with reflection

---
+++ C# OOP/ReflectionAndAttributes/Demo/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Person
+    {
+        private void Eat()
+        {
+
+        }
+
+        private void Sleep()
+        {
+            
+        }
+
+        public void Work()
+        {
+
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Type classType = typeof(Person);
+            var methods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+            foreach (var method in methods)
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
+    }
+}
