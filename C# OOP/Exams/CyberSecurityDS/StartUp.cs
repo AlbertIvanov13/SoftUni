72b3486ca81110ac6e429452b297dda9a7023bc8
Albert Ivanov <albertivanov187@gmail.com>
2025-03-28 21:06:04 +0200

    Creating folders structure

---
+++ C# OOP/Exams/CyberSecurityDS/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace CyberSecurityDS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
