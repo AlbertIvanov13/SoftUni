ec12b33c4117afcfad74b94ed1f1ffbe552cdb1a
Albert Ivanov <albertivanov187@gmail.com>
2025-04-03 22:35:51 +0300

    Implementing PhishingAttack class

--- C# OOP/Exams/CyberSecurityDS/Models/PhishingAttack.cs
+++ C# OOP/Exams/CyberSecurityDS/Models/PhishingAttack.cs
@@ -9,6 +9,30 @@ namespace CyberSecurityDS.Models
 {
     public class PhishingAttack : CyberAttack
     {
-        
+        private string targetMail;
+        public PhishingAttack(string attackName, int severityLevel, string targetMail) : base(attackName, severityLevel)
+        {
+            TargetMail = targetMail;
+        }
+
+        public string TargetMail
+        {
+            get { return targetMail; }
+            private set 
+            { 
+                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.TargetMailRequired);
+                }
+                targetMail = value; 
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Attack: {AttackName}, Severity: {SeverityLevel} (Target Mail: {TargetMail})");
+            return sb.ToString().Trim();
+        }
     }
 }
