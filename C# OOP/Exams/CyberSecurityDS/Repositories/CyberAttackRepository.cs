2b5c73ff698afcab1c2eac1067439e39efc4d157
Albert Ivanov <albertivanov187@gmail.com>
2025-04-04 21:51:59 +0300

    Implementing CyberAttackRepository

--- C# OOP/Exams/CyberSecurityDS/Repositories/CyberAttackRepository.cs
+++ C# OOP/Exams/CyberSecurityDS/Repositories/CyberAttackRepository.cs
@@ -11,6 +11,38 @@ namespace CyberSecurityDS.Repositories
 {
     public class CyberAttackRepository : IRepository<ICyberAttack>
     {
-        
+        private readonly List<ICyberAttack> models;
+
+        public CyberAttackRepository()
+        {
+            models = new List<ICyberAttack>();
+        }
+        public IReadOnlyCollection<ICyberAttack> Models { get { return models.AsReadOnly(); } }
+
+        public void AddNew(ICyberAttack model)
+        {
+            models.Add(model);
+        }
+
+        public bool Exists(string name)
+        {
+            if (models.Any(a => a.AttackName == name))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public ICyberAttack GetByName(string name)
+        {
+            if (models.Any(a => a.AttackName == name))
+            {
+                ICyberAttack attack = models.FirstOrDefault(a => a.AttackName == name);
+                return attack;
+            }
+
+            return null;
+        }
     }
 }
