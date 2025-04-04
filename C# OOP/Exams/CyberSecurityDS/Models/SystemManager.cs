e5f9ff819ba9d0b162c4707aa755f75856d594dd
Albert Ivanov <albertivanov187@gmail.com>
2025-04-04 21:46:21 +0300

    Implementing SystemManager class

--- C# OOP/Exams/CyberSecurityDS/Models/SystemManager.cs
+++ C# OOP/Exams/CyberSecurityDS/Models/SystemManager.cs
@@ -11,6 +11,25 @@ namespace CyberSecurityDS.Models
 {
     public class SystemManager : ISystemManager
     {
-        
+        private IRepository<ICyberAttack> cyberAttacks;
+        private IRepository<IDefensiveSoftware> defensiveSoftwares;
+
+        public SystemManager()
+        {
+            CyberAttacks = new CyberAttackRepository();
+            DefensiveSoftwares = new DefensiveSoftwareRepository();
+        }
+
+        public IRepository<ICyberAttack> CyberAttacks
+        {
+            get { return cyberAttacks; }
+            private set { cyberAttacks = value; }
+        }
+
+        public IRepository<IDefensiveSoftware> DefensiveSoftwares
+        {
+            get { return defensiveSoftwares; }
+            private set { defensiveSoftwares = value; }
+        }
     }
 }
