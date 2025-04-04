86f71ee4f5890fdcf7d637f88ceef15ebd3b0bbf
Albert Ivanov <albertivanov187@gmail.com>
2025-04-04 21:53:50 +0300

    Implementing DefensiveSoftwareRepository

--- C# OOP/Exams/CyberSecurityDS/Repositories/DefensiveSoftwareRepository.cs
+++ C# OOP/Exams/CyberSecurityDS/Repositories/DefensiveSoftwareRepository.cs
@@ -10,6 +10,38 @@ namespace CyberSecurityDS.Repositories
 {
     public class DefensiveSoftwareRepository : IRepository<IDefensiveSoftware>
     {
-        
+        private readonly List<IDefensiveSoftware> models;
+
+        public DefensiveSoftwareRepository()
+        {
+            models = new List<IDefensiveSoftware>();
+        }
+        public IReadOnlyCollection<IDefensiveSoftware> Models { get { return models.AsReadOnly(); } }
+
+        public void AddNew(IDefensiveSoftware model)
+        {
+            models.Add(model);
+        }
+
+        public bool Exists(string name)
+        {
+            if (models.Any(d => d.Name == name))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public IDefensiveSoftware GetByName(string name)
+        {
+            if (models.Any(d => d.Name == name))
+            {
+                IDefensiveSoftware defensiveSoftware = models.FirstOrDefault(d => d.Name == name);
+                return defensiveSoftware;
+            }
+
+            return null;
+        }
     }
 }
