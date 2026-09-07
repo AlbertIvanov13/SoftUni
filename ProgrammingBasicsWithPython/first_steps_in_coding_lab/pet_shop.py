c5bca764ff0cdd32d0aa37a1564060a4eba16933
Albert Ivanov <albertivanov187@gmail.com>
2026-09-07 17:40:22 +0300

    Pet shop

---
+++ ProgrammingBasicsWithPython/first_steps_in_coding_lab/pet_shop.py
@@ -0,0 +1,9 @@
+dog_pack_count = int(input())
+cat_pack_count = int(input())
+
+dog_pack_price = 2.50
+cat_pack_price = 4.00
+
+total_amount = dog_pack_count * dog_pack_price + cat_pack_count * cat_pack_price
+
+print(f"{total_amount} lv.")
